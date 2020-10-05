ffbc404c0b7088869fae28a7d4579ca6f5c7815c
timoan <47566744+timoan@users.noreply.github.com>
2020-10-05 13:42:21 +0300

    Add files via upload

---
+++ 5_Strategy_Sort/Strategy/BubleSort.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    class BubleSort : StrategySort
+    {
+        public BubleSort()
+        {
+            Title = "Сортировка пузырьком";
+        }
+        public override string ToString()
+        {
+            return Title;
+        }
+        public override void Sort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ 5_Strategy_Sort/Strategy/Context.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    class Context
+    {
+        StrategySort strategy;
+        int[] array;
+
+        public Context(StrategySort strategy, int[] array)
+        {
+            this.strategy = strategy;
+            this.array = array;
+        }
+
+        public void Sort() { strategy.Sort(array); }
+
+        public void PrintArray()
+        {
+            Console.WriteLine(strategy.ToString());
+            for (int i = 0; i < array.Length; i++)
+                Console.Write(array[i] + " ");
+            Console.WriteLine();
+        }
+    }
+}
